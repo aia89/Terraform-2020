723a16d485320d0be230c5baa70eb3fc8fe67a48
aia89 <aiadosbaeva@gmail.com>
2020-06-24 01:37:12 +0000

    terraform_modules_auto_scaling_group

---
+++ class4/apps.tf
@@ -0,0 +1,8 @@
+module "app1" {
+source = "./module"
+region = "us-east-1"
+image_owner = "137112412989"  
+}
+
+
+#if there is a request to create a new asg then copy paste lines rename it and run.
