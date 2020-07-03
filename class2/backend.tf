2f37a9bab80b769bfb8cf67b5af7288a1c2b1f00
aia89 <aiadosbaeva@gmail.com>
2020-07-03 00:59:25 +0000

    output.tf added into class2. collaboration

--- class2/backend.tf
+++ class2/backend.tf
@@ -1,7 +1,12 @@
 terraform {
-backend "s3" {
-bucket = "terraform-state-aipril-class-aia"
-key = "jenkins/us-east-1/tools/virginia/jenkins.tfstate"
-region = "us-east-1"
+
+  required_version  =  "0.11.14"
+  
+  backend "s3" {
+  bucket = "terraform-state-aipril-class-aia"
+  key = "jenkins/us-east-1/tools/virginia/jenkins.tfstate"
+  region = "us-east-1"
+  #dynamodb_table = "prod"
+
   }
 }
